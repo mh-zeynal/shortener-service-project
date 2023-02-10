477f9b6ff2b70a6c04c750698d6321e78147d0b5
mh-zeynal <mh.zeynal@gmail.com>
2023-02-10 15:52:14 +0330

    chore: remove gorm-related functions

--- back-end/db/mysql.go
+++ back-end/db/mysql.go
@@ -1,11 +1,11 @@
 package db
 
 import (
+	"back-end/utils"
+	"database/sql"
 	"fmt"
-	"gorm.io/gorm"
-	"gorm.io/driver/mysql"
-	"time"
-	"back-end/model"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var DB *sql.DB
@@ -24,75 +24,4 @@ func MakeDbConnection() {
 	if err != nil {
 		fmt.Println("database connection has failed")
 	}
-}
-
-func AddNewRow(short string, long string)  {
-	temp := model.URL{Short: short, Long: long, Date: time.Now().String()}
-	_ = db.Create(&temp)
-}
-
-func DeleteRow(id int)  {
-	_ = db.Delete(&model.URL{}, id)
-}
-
-func GetAllRows() *[]model.URL {
-	rows := make([]model.URL, 0)
-	_ = db.Find(&rows)
-	return &rows
-}
-
-func GetRowViaLongUrl(long string) model.URL {
-	temp := model.URL{}
-	rows, _ := db.Model(&model.URL{}).Rows()
-	defer rows.Close()
-	for rows.Next() {
-		var user model.URL
-		_ = db.ScanRows(rows, &user)
-		if user.Long == long {
-			temp = user
-			break
-		}
-	}
-	return temp
-}
-
-func GetRowViaShortUrl(short string) model.URL {
-	temp := model.URL{}
-	rows, _ := db.Model(&model.URL{}).Rows()
-	defer rows.Close()
-	for rows.Next() {
-		var user model.URL
-		_ = db.ScanRows(rows, &user)
-		if user.Short == short {
-			temp = user
-			break
-		}
-	}
-	return temp
-}
-
-func IsLongAvailable(long string) bool {
-	rows, _ := db.Model(&model.URL{}).Rows()
-	defer rows.Close()
-	for rows.Next() {
-		var user model.URL
-		_ = db.ScanRows(rows, &user)
-		if user.Long == long {
-			return true
-		}
-	}
-	return false
-}
-
-func IsShortAvailable(short string) bool {
-	rows, _ := db.Model(&model.URL{}).Rows()
-	defer rows.Close()
-	for rows.Next() {
-		var user model.URL
-		_ = db.ScanRows(rows, &user)
-		if user.Short == short {
-			return true
-		}
-	}
-	return false
 }
