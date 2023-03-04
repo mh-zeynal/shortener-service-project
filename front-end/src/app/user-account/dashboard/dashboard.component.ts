38f15cd3fd9fab93bc10e0792c1ddc6632abdc40
mh-zeynal <mh.zeynal@gmail.com>
2023-03-04 20:52:33 +0330

    refactor: rewrite active button styling previous version was depended to angular id-tag, but this one depends on current route of user dashboard

--- front-end/src/app/user-account/dashboard/dashboard.component.ts
+++ front-end/src/app/user-account/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -10,16 +11,19 @@ export class DashboardComponent implements OnInit {
 
   isUserDropdownOpen = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  markAsSelected(targetButton: Element){
+  markAsSelected(){
     let button = document.getElementsByClassName('active-dashboard-button');
     while (button.length)
       button[0].classList.remove('active-dashboard-button');
-    targetButton.classList.add('active-dashboard-button');
+    let userRoute = this.router.url.replace('/user/', '');
+    let activeButton = document.getElementById(userRoute);
+    if (activeButton)
+      activeButton.classList.add('active-dashboard-button');
   }
 
 }
